3e40a26030aea999fd4ad1e9b2ecb65038a07b68
Tran Tinh <tranngoctinh96@gmail.com>
2019-12-21 17:21:51 +0700

    get/{id}
    Auto reset id

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React ,{useEffect , useState}from "react";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "react-bootstrap/Navbar"
-import MouseContainer from "./components/mouseContainer"
-import Count from "./components/detail/count"
+//import MouseContainer from "./components/mouseContainer"
+//import Count from "./components/detail/count"
+import DataFeching from "./components/Fetching/DataFeching"
 
 function App() {
  
   return (
-    <>
+    <div className="container">
       <Navbar expand="lg" variant="dark" bg="light">
           <Navbar.Brand href="#">Navbar</Navbar.Brand>
       </Navbar>
       {/*<MouseContainer/>*/ }
-      <Count/>
-    </>
+      <DataFeching/>
+    </div>
   );
 }
 
